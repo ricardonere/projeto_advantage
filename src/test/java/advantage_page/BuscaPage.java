cb1b901dfed906b32a879380e25f1536f08a3b1c
Ricardo <ricardonere@gmail.com>
2021-08-14 21:52:53 -0300

    Adicao JUnit-Selenium no pom/criacao classe testes

---
+++ src/test/java/advantage_page/BuscaPage.java
@@ -0,0 +1,16 @@
+package advantage_page;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BuscaPage {
+
+	public BuscaPage(ChromeDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
